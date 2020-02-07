6cb3df790bdfd60d15c9324c5185b68b8af4c2ee
Sparcmen JS <alexandr.pratsiovytyi@gmail.com>
2020-02-07 15:53:52 +0200

    section about is done

--- #source/js/slider.js
+++ #source/js/slider.js
@@ -1,15 +1,14 @@
 if ($('.slider__body').length>0){
-    console.log("slick work");
     
     $('.slider__body').slick({
         autoplay: true,
         infinite: false,
         dots: false,
-        arrows: false,
+        // arrows: true,
         accesibility: false,
         slidesToShow: 1,
         autoplaySpeed: 3000,
-        adaptiveHeight: true,
+        adaptiveHeight: false,
         nextArrow: '<button type="button" class="slick-next"></button>',
         prevArrow: '<button type="button" class="slick-prev"></button>',
         responsive:[{
