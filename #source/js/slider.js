e29e8ae8a33d500a34143ed7bc191e3497982ea7
Sparcmen JS <alexandr.pratsiovytyi@gmail.com>
2020-02-08 23:33:18 +0200

    little slider test

--- #source/js/slider.js
+++ #source/js/slider.js
@@ -19,4 +19,40 @@ if ($('.slider__body').length>0){
     });
 }
 
+if ($('.service__carousel').length>0){
+    
+    $('.service__carousel').slick({
+        autoplay: true,
+        infinite: false,
+        dots: false,
+        arrows: false,
+        accesibility: false,
+        slidesToShow: 4,
+        autoplaySpeed: 3000,
+        adaptiveHeight: false,
+        nextArrow: '<button type="button" class="slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        responsive:[            
+            {
+                breakpoint:1300,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint:965,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint:660,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+}
+
 
