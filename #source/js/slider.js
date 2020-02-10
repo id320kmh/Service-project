4779f08626dc5d7bc56733c085d1478ff7acfc11
Sparcmen JS <alexandr.pratsiovytyi@gmail.com>
2020-02-10 23:05:44 +0200

    secons slider is fixed

--- #source/js/slider.js
+++ #source/js/slider.js
@@ -56,3 +56,42 @@ if ($('.service__carousel').length>0){
 }
 
 
+if ($('.service__item-row').length>0){
+
+    $('.service__item-row').slick({
+        autoplay: true,
+        infinite: false,
+        dots: false,
+        arrows: true,
+        accesibility: false,
+        slidesToShow: 3,
+        autoplaySpeed: 3000,
+        adaptiveHeight: false,
+        appendArrows: '.service__control-arrow',
+        prevArrow: '<span class="ibg"><button type="button" class="service__btn service__btn_left"></button></span>',
+        nextArrow: '<span class="ibg"><button type="button" class="service__btn service__btn_right"></button></span>',
+        // Cами кнопки не нужно дублировать в html. Просто создаем для них контейнер, и указываем в <appendArrows> этот контейнер. Когда мы указываем <prevArrow> и <nextArrow>, тем самым мы создаем и вставляем эти кнопки в данный контейнер!
+        responsive:[            
+            {
+                breakpoint:1300,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint:965,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint:660,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+}
+
+
